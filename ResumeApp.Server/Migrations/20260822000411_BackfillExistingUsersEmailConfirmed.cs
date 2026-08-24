55d3e986a3ac181e60a1d45a719a2779af3dc130
wisdomf41 <48020427+wisdomf41@users.noreply.github.com>
2026-08-24 03:57:56 +0100

    Backfill email confirmation for existing users

---
+++ ResumeApp.Server/Migrations/20260822000411_BackfillExistingUsersEmailConfirmed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillExistingUsersEmailConfirmed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Updated: Preserve access for users created before email-confirmation enforcement.
+            migrationBuilder.Sql(
+                """
+                UPDATE [AspNetUsers]
+                SET [EmailConfirmed] = CAST(1 AS bit)
+                WHERE [EmailConfirmed] = CAST(0 AS bit);
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
